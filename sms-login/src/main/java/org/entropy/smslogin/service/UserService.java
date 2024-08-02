6484e96b55f711e1db50e8b4b2886bdd74c855ab
s-chance <entropy.tree@outlook.com>
2024-08-03 00:37:52 +0800

    fix(userservice): fix the verify code reuse vulnerability

--- sms-login/src/main/java/org/entropy/smslogin/service/UserService.java
+++ sms-login/src/main/java/org/entropy/smslogin/service/UserService.java
@@ -7,9 +7,11 @@ import org.entropy.smslogin.db.UserDB;
 import org.entropy.smslogin.dto.LoginFormDTO;
 import org.entropy.smslogin.pojo.Result;
 
+import java.util.Objects;
+
 @Slf4j
 public class UserService {
-    
+
     private final UserDB userDB = new UserDB();
 
     public Result<?> sendCode(String phone, HttpSession session) {
@@ -20,7 +22,8 @@ public class UserService {
         }
         // 3.符合，生成验证码
         String code = RandomUtil.randomNumbers(6);
-        // 4.保存验证码到session
+        // 4.保存原始手机号和验证码到session
+        session.setAttribute("originalPhone", phone);
         session.setAttribute("code", code);
         // 5.发送验证码
         log.debug("发送短信验证码：{}", code);
@@ -31,9 +34,15 @@ public class UserService {
         // 1.校验手机号
         String phone = loginFormDTO.getPhone();
         if (phone.length() != 11) {
-            // 2.不符合，返回错误信息
+            // 不符合，返回错误信息
             return Result.failure("手机号不正确");
         }
+        // 防止验证码重用攻击，校验手机号是否为接收验证码的手机号
+        String originalPhone = (String) session.getAttribute("originalPhone");
+        if (!Objects.equals(originalPhone, phone)) {
+            return Result.failure("该手机号与接收验证码的手机号不一致，请确认接收验证码的手机号");
+        }
+
         // 2.校验验证码
         String cacheCode = (String) session.getAttribute("code");
         String code = loginFormDTO.getCode();
