23728d5260b61ba9a7f0ecd0fc391b1592c4d8bf
s-chance <entropy.tree@outlook.com>
2024-08-14 20:52:50 +0800

    feat(*): using the lua script to execute atomic operations in redis

--- coupon-sec-kill/src/main/java/org/entropy/couponseckill/utils/SimpleRedisLock.java
+++ coupon-sec-kill/src/main/java/org/entropy/couponseckill/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package org.entropy.couponseckill.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -12,6 +15,13 @@ public class SimpleRedisLock implements ILock {
 
     private static final String PREFIX = "couponseckill:lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("scripts/unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -27,6 +37,16 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
+        // 调用 lua 脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
+    }
+
+    /*@Override
+    public void unlock() {
         // 获取线程标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁中的标识
@@ -36,5 +56,5 @@ public class SimpleRedisLock implements ILock {
             // 释放锁
             stringRedisTemplate.delete(PREFIX + name);
         }
-    }
+    }*/
 }
