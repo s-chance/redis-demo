6f8155382e0d561644a5faf43b247248a2033455
s-chance <entropy.tree@outlook.com>
2024-08-27 01:15:50 +0800

    feat(*): blog-comment init

---
+++ blog-comment/src/test/java/org/entropy/blogcomment/BlogCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.entropy.blogcomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
